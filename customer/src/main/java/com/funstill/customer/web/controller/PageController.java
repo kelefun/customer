a59ffd6b2f2aa048a0281b7994813eb1c02e8ba7
kelefun <kelefun@foxmail.com>
2017-08-22 15:23:21 +0800

    update

--- customer/src/main/java/com/funstill/customer/web/controller/PageController.java
+++ customer/src/main/java/com/funstill/customer/web/controller/PageController.java
@@ -3,8 +3,6 @@ package com.funstill.customer.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.funstill.customer.module.model.CustomerQuery;
-
 /**
  * @author liukaiyang
  * @version v1.0.0
@@ -13,9 +11,12 @@ import com.funstill.customer.module.model.CustomerQuery;
 public class PageController {
 
     @GetMapping(value={"/","/index"})
-    public String index(CustomerQuery query) {
+    public String index() {
         return "view/index.html";
     }
-    
+    @GetMapping(value={"/login"})
+    public String login() {
+        return "view/login.html";
+    }
 
 }
